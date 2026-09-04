07e1fc1fb53b3da0a7452663e5e60ffc46331d39
Aukrit Kumapirun <aukrit.ku@ku.th>
2026-09-04 10:20:04 +0700

    4/9/2569

--- labs/week10-lab/check-password.py
+++ labs/week10-lab/check-password.py
@@ -8,7 +8,27 @@
 #your password is not strong!
 #please input your password:Boonchoo@123
 #your password is strong!
-password = input("please input your password:")
+def check_password():
+    password = input("please input your password:")
+
+    count_digit = 0
+    count_alpha = 0
+    count_at = 0
+
+    for ch in password:
+        if ch.isdigit():
+            count_digit += 1
+        if ch.isalpha():
+            count_alpha += 1
+        if ch == '@':
+            count_at += 1
+
+    if count_digit > 0 and count_alpha > 0 and count_at == 1 and len(password) > 8:
+        print("your password is strong!")
+    else:
+        print("your password is not strong!")
+
+check_password()
 
 
 
